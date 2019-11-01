ab500b8dd1e3e52aca6cb9f47617b53ce485445d
Elle McFarlane <ellesummer212@gmail.com>
2019-10-31 22:55:38 -0400

    Update duolingoipa.user.js
    Deleted dead code and changed function names for clarity

--- duolingoipa.user.js
+++ duolingoipa.user.js
@@ -9,66 +9,67 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://gist.github.com/ellemcfarlane/5a9dd00c7a0290415848f745ecd97917/raw/duolingoipa.user.js
 // ==/UserScript==
-
 // class for Duolingo translate challenge
 let TRANSLATE_CHALLENGE = "_3VFHG"
+
+/* Functions for IPA display */
 
 // creates url from each word on page and passes to multiAjax for full IPA display
 function showIPA() {
     // create urls for each word on screen
         words = document.getElementsByClassName("TyDwn ");
+    // create empty string fullIPA to pass to muliAjax
         var fullIPA = "";
-        var IPA = "";
-        var wordstrs = "";
         var trueLen = words.length - 1
         var urls = new Array(trueLen);
         for (var i = 0; i < trueLen; i++) {
             var word = words[i].innerText;
-            wordstrs += word + " ";
             var url = "https://api.pons.com/v1/dictionary?q=" + word + "&l=dees&in=de";
             urls[i] = url;
         }
     // get IPA for each word
-        multiAjax(urls, fullIPA);
+        createIPAs(urls, fullIPA);
 }
 
-// adds html element of the IPA for the sentence to the screen
-function multiAjax (urls, fullIPA) {
+// adds html element of the IPA to the screen
+function createIPAs (urls, fullIPA) {
         if (urls.length == 0) {
             var para = document.createElement("p");
             var node = document.createTextNode(fullIPA);
             para.appendChild(node);
             var element = document.getElementsByClassName("_23gwq")[0];
-            console.log(element);
             var space = document.createElement("div");
             space.classList.add("_IPA_");
             space.appendChild(para);
             element.appendChild(space);
         }
         else {
-            // use ajax for asynch request of url popped from urls list
-            ajax(urls.pop())
+            // request data containing IPA text for url popped from urls list
+            getIPAText(urls.pop())
                 .then(function(result) {
                 var data = JSON.parse(result)
                 var el = document.createElement( 'html' );
                 el.innerHTML = data[0].hits[0].roms[0].headword_full;
+                // get IPA text and add to string fullIPA
                 var phonetics = el.getElementsByClassName( 'phonetics' );
                 if (phonetics.length > 0) {
                     var IPA = phonetics[0].innerText + " ";
                     fullIPA = IPA + fullIPA;
                 }else{
+                    // IPA not available for word
                     fullIPA = "n/a " + fullIPA;
                 }
-                multiAjax(urls, fullIPA);
+                // recursively get IPA for next url
+                createIPAs(urls, fullIPA);
             })
                 .catch(function() {
-                console.log("error multiAjax");
+                console.log("error createIPAs");
             });
         }
 }
 
 // uses GM.xmlHTTpRequest to get definition object from PONS and return it in Promise object
-function ajax(url) {
+function getIPAText(url) {
   return new Promise(function(resolve, reject) {
     GM.xmlHttpRequest({
         method: "GET",
@@ -81,8 +82,9 @@ function ajax(url) {
   });
 }
 
-// calls showIPA to display the IPA when translate challenge is detected
+// oldclass is used to check if challenge is already detected (so onChallenge mutation is not called infinitely)
 var oldclass = "";
+// calls showIPA to display the IPA when translate challenge is detected
 function onChallenge(mutations) {
     var newclass = "";
     // get the translate challenge class if exists
